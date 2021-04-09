2d43ae433b2df0058d3d3a18d3e57c80a58e6101
Xuewei Zhang <xuewei@observeinc.com>
2021-04-09 00:13:26 -0700

    Stop lexing when hitting EOF

--- parse_test.go
+++ parse_test.go
@@ -4,6 +4,7 @@ package mustache
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -96,6 +97,24 @@ func TestParser(t *testing.T) {
 			if !reflect.DeepEqual(elem, test.expected[i]) {
 				t.Errorf("elements are not equal %v != %v", elem, test.expected[i])
 			}
+		}
+	}
+}
+
+func TestParserNegative(t *testing.T) {
+	for _, test := range []struct {
+		template string
+		expErr   string
+	}{
+		{
+			"{{foo}",
+			`1:6 syntax error: unreachable code t_error:"unclosed tag"`,
+		},
+	} {
+		parser := newParser(newLexer(test.template, "{{", "}}"))
+		_, err := parser.parse()
+		if err == nil || !strings.Contains(err.Error(), test.expErr) {
+			t.Errorf("expect error: %q, got %q", test.expErr, err)
 		}
 	}
 }
